22f5d683dcf8e97d8ba821b2f02a5963e11d0e8b
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-01 13:17:53 +0100

    Class Components set up

---
+++ github-card/src/components/Followers.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Followers extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+
+  return <div className="Followers">
+    Followers
+  </div>;
+  }
+}
+
+export default Followers;
---
+++ github-card/src/components/GithubUser.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class GithubUser extends React.Component {
+  constructor(){
+    super();
+  }
+  render(){
+
+  return <div className="GithubUser">
+    GithubUser
+  </div>;
+  }
+}
+
+export default GithubUser;
