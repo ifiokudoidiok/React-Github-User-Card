de7ef0f7596f4f767b539b8d0fc8cb95e88d3d2d
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-01 16:08:11 +0100

    User component completed

--- github-card/src/components/GithubUser.js
+++ github-card/src/components/GithubUser.js
@@ -1,7 +1,44 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const UserStyle = styled.div`
+  max-width: 800px;
+  margin: 50px auto;
+  padding: 30px;
+ border: 0.1rem solid black;
+ background: grey;
+  display: flex;
+  align-items: center;
+h1{
+  display:block;
+  color:white;
+  font-size:2rem;
+  text-align:left;
+  
+}
+span{
+  color:black;
+  font-size:1.5rem;
+  font-weight: normal;
+}
+  /* text-align: center; */
+  /* width: 50%; */
+  img{
+    padding-right: 30px;
+  max-width: 200px;
+}
+  /* flex-direction: column; */
+`;
+const Info = styled.div`
+
+`;
+
 
 const userAPI = "https://api.github.com/users/ifiokudoidiok";
+const options = {
+  headers: { Authorization: 'fd1d7881204da2a1712d521b38c2fa6b5ff4510d' }
+};
 
 export default class GithubUser extends React.Component {
   constructor(props) {
@@ -19,7 +56,7 @@ export default class GithubUser extends React.Component {
 
   componentDidMount() {
     axios
-      .get(userAPI)
+      .get(userAPI, options)
       .then(response => {
         this.setState({
           user: {
@@ -37,13 +74,16 @@ export default class GithubUser extends React.Component {
   }
   render() {
     return (
-      <div>
-        {/* <img src={this.state.user.userImage} /> */}
-        <h1>Name: {this.state.user.username}</h1>
-        <h1>Username: {this.state.user.userAlias}</h1>
-        <h1>Bio: {this.state.user.userBio}</h1>
-        <h1>Number of followers:{this.state.user.userFollowers}</h1>
-      </div>
+      <UserStyle>
+        <img src={this.state.user.userImage} />
+        <Info>
+
+          <h1>Name: <span> {this.state.user.username}</span></h1>
+          <h1>Username: <span> {this.state.user.userAlias}</span></h1>
+          <h1>Bio: <span> {this.state.user.userBio}</span></h1>
+          <h1>Number of followers: <span> {this.state.user.userFollowers}</span></h1>
+        </Info>
+      </UserStyle>
     );
   }
 }
