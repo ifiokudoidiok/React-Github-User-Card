8cdf69f10f516dd698432ced338d8b95155f2169
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-10-01 14:24:24 +0100

    Displayed Users Details successfully

--- github-card/src/components/GithubUser.js
+++ github-card/src/components/GithubUser.js
@@ -1,15 +1,49 @@
 import React from "react";
+import axios from "axios";
 
-class GithubUser extends React.Component {
-  constructor(){
-    super();
+const userAPI = "https://api.github.com/users/ifiokudoidiok";
+
+export default class GithubUser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        userImage: "",
+        userAlias: "",
+        userBio: "",
+        userLocation: "",
+        userFollowers: ""
+      }
+    };
   }
-  render(){
 
-  return <div className="GithubUser">
-    GithubUser
-  </div>;
+  componentDidMount() {
+    axios
+      .get(userAPI)
+      .then(response => {
+        this.setState({
+          user: {
+            username: response.data.name,
+            userImage: response.data.avatar_url,
+            userAlias: response.data.login,
+            userBio: response.data.bio,
+            userFollowers: response.data.followers
+          }
+        });
+      })
+      .catch(response => {
+        debugger;
+      });
+  }
+  render() {
+    return (
+      <div>
+        {/* <img src={this.state.user.userImage} /> */}
+        <h1>Name: {this.state.user.username}</h1>
+        <h1>Username: {this.state.user.userAlias}</h1>
+        <h1>Bio: {this.state.user.userBio}</h1>
+        <h1>Number of followers:{this.state.user.userFollowers}</h1>
+      </div>
+    );
   }
 }
-
-export default GithubUser;
